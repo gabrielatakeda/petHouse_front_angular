8e691b47d985d3972f6dec067fd80b4dd74dc6b1
cwtspace <joaovitorserrabatista47@gmail.com>
2025-09-19 19:01:23 -0300

    adicionando landing page

---
+++ src/app/components/promo-section/promo-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-promo-section',
+  imports: [],
+  templateUrl: './promo-section.component.html',
+  styleUrl: './promo-section.component.scss'
+})
+export class PromoSectionComponent {
+
+}
