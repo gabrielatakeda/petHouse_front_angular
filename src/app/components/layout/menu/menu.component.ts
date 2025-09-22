cae1864a2ef48b455306f68c727df343dd409e30
JLMartinazzo <jl.martinazzo@gmail.com>
2025-09-21 22:14:54 -0300

    tela usuario

---
+++ src/app/components/layout/menu/menu.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MdbCollapseModule } from 'mdb-angular-ui-kit/collapse';
+
+@Component({
+  selector: 'app-menu',
+  imports: [MdbCollapseModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
