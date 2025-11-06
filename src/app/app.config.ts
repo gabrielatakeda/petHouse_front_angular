a7a8d6aec722e409901b5e35a742e70e98fea18a
neulandp <neulandp074@gmail.com>
2025-11-06 20:18:57 -0300

    Responsividade adicionada na tela de recuperação de senha.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations, provideNoopAnimations } from '@angular/platform-browser/animations'; 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
@@ -11,5 +11,7 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
     provideHttpClient(),
-    provideAnimations() ] // NECESSARIO PARA FAZER REQUISIÇÕES HTTP - AGE COMO UM RESTCONTROLLER DO JAVA
+    provideAnimations() // NECESSARIO PARA FAZER REQUISIÇÕES HTTP - AGE COMO UM RESTCONTROLLER DO JAVA
+    //provideHttpClient(withInterceptors([meuhttpInterceptor])) //Habilitando interceptador para fazer a comunicaçãi
+  ] 
   }
