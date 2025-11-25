ddee8a94ad41e5ec359b4da4f96452dcc0c7ae27
JLMartinazzo <jl.martinazzo@gmail.com>
2025-11-25 20:16:18 -0300

    vendo se o commit ta de boa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PromoSectionComponent } from './components/promo-section/promo-section.component';
 
+
+//teste
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
