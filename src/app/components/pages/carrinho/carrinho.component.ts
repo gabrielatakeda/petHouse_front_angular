46508f879faff604ca5689d5f9efecb556d2e029
Debora <dfgroto@gmail.com>
2025-11-26 20:29:34 -0300

    ultimas atualizacoes

--- src/app/components/pages/carrinho/carrinho.component.ts
+++ src/app/components/pages/carrinho/carrinho.component.ts
@@ -1,18 +1,70 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';  // 
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
+import { EnderecoService } from '../../../services/endereco.service';
 
 @Component({
   selector: 'app-carrinho',
   standalone: true,
-  imports: [CommonModule, FormsModule], // 
+  imports: [CommonModule, FormsModule, RouterLink], // 
   templateUrl: './carrinho.component.html',
   styleUrls: ['./carrinho.component.scss']
 })
+
 export class CarrinhoComponent {
 
-cartItems: any[] = [];
+  pagamentoSelecionado: string = '';
 
-pagamentoSelecionado: string = '';
-  
+  erroFinalizacao: string | null = null;
+  enderecosAbertos = false;
+  mostrarModalEndereco = false;
+  erroEndereco: string | null = null;
+
+  enderecos: any[] = [];
+
+  enderecoSelecionado: any = null;       // endereço final selecionado
+  enderecoTempSelecionado: any = null;   // usado enquanto o usuário está escolhendo
+
+  constructor(private enderecoService: EnderecoService) { }
+
+  ngOnInit(): void {
+    this.carregarEnderecos();
+  }
+
+  carregarEnderecos() {
+    this.enderecoService.findAll().subscribe(data => {
+      this.enderecos = data;
+    });
+  }
+  toggleEnderecos() {
+    this.enderecosAbertos = !this.enderecosAbertos;
+  }
+
+  confirmarEndereco() {
+    if (!this.enderecoTempSelecionado) {
+      this.erroEndereco = "Selecione um endereço antes de continuar.";
+      return;
+    }
+
+    this.enderecoSelecionado = this.enderecoTempSelecionado;
+    this.enderecosAbertos = false;
+    this.erroEndereco = null;
+  }
+
+ finalizarCompra() {
+  this.erroFinalizacao = null; // reset erro
+
+  // Verificar endereço
+  if (!this.enderecoSelecionado) {
+    this.enderecosAbertos = true;
+    this.erroEndereco = "Selecione um endereço antes de finalizar.";
+  }
+
+  // Verificar forma de pagamento
+  if (!this.pagamentoSelecionado) {
+    this.erroFinalizacao = "Selecione uma forma de pagamento antes de finalizar.";
+  }
+}
+
 }
